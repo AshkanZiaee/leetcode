ba02fac3293e2f7654f7aa20f2b2cfda898fa095
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-02 00:10:46 +0330

    11th day part 2

--- atoi-review.js
+++ atoi-review.js
@@ -10,18 +10,22 @@ var myAtoi = function (s) {
     }
   }
 
+  if (s[index] === "-" || s[index] === "+") {
+    isNeg = s[index] === "-"
+    index++
+  }
   for (let i = index; i < s.length; i++) {
-    if (s[i] === "-") isNeg = true
     let num = s.charCodeAt(i) - 48
-    if (num < 0 || num > 9) break
-    // res *= 10
+    if (num < 0 || num > 9) {
+      break
+    }
+    res *= 10
     res += num
   }
   let min = -(2 ** 31)
-  let max = 2 ** 31
+  let max = 2 ** 31 - 1
   res = res > max ? max : res < min ? min : res
-  console.log("res", res)
   return isNeg ? -res : res
 }
 
-console.log(myAtoi("-14984"))
+console.log(myAtoi("+1"))

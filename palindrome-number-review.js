88d54a6126289cc7142c7e1a0bbf4d745113b6db
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-03 00:37:00 +0330

    12th day

---
+++ palindrome-number-review.js
@@ -0,0 +1,10 @@
+var isPalindrome = function (x) {
+  let stringifiedNumber = String(x)
+  let res = ""
+  for (let i = stringifiedNumber.length - 1; i >= 0; i--) {
+    res += stringifiedNumber[i]
+  }
+  return res === stringifiedNumber
+}
+
+console.log(isPalindrome(131))

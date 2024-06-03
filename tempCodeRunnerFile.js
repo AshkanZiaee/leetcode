04f6dfe15893cba89d103ae3bafcc390cfa03698
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-03 23:45:41 +0330

    13th day

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,2 @@
-45
+    let area = (right - left) * Math.min(height[left], height[right])
+    maxArea = Math.max(area, maxArea)
---
+++ water-container-review.js
@@ -0,0 +1,19 @@
+var maxArea = function (height) {
+  let left = 0
+  let right = height.length - 1
+  let maxArea = 0
+  while (left < right) {
+    let area = (right - left) * Math.min(height[left], height[right])
+    maxArea = Math.max(area, maxArea)
+    if (height[left] < height[right]) left++
+    else {
+      right--
+    }
+  }
+
+  return maxArea
+}
+
+const height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+console.log(maxArea(height))

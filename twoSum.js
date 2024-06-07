4f93b0b7d55ddaaf84a5478b17bbdd88e86777c8
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-07 23:44:30 +0300

    17th day

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,11 @@
 function twoSum(nums, target) {
-  const map = new Map()
+  let myMap = new Map()
+
   for (let i = 0; i < nums.length; i++) {
     let complementary = target - nums[i]
-    if (map.has(complementary)) {
-      return [i, map.get(complementary)]
-    }
-    map.set(nums[i], i)
+    if (myMap.has(complementary)) return [i, myMap.get(complementary)]
+    myMap.set(nums[i], i)
   }
-  return -1
 }
 
-console.log(twoSum([2, 7, 11, 15], 15))
+console.log(twoSum([2, 7, 11, 15], 9))

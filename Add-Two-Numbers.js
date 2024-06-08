664313f0e06c6c97b3f6af11b1253a6cb1248281
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-08 22:16:46 +0330

    18th day

--- Add-Two-Numbers.js
+++ Add-Two-Numbers.js
@@ -1,32 +1,26 @@
-class ListNode {
-  constructor(val, next) {
-    this.val = val === undefined ? 0 : val
-    this.next = next === undefined ? null : next
-  }
-}
 
+}
 var addTwoNumbers = function (l1, l2) {
-  const dummyHead = new ListNode(0)
-  let current = dummyHead
-  let carry = 0
-  while (l1 !== null || l2 !== null) {
-    let sum = carry
-    if (l1) {
-      sum += l1.val
-      l1 = l1.next
+  let dumbyNode = new ListNode(0)
+  let current = dumbyNode
+    let carry = 0
+    while (l1 !== null || l2 !==null) {
+      let total = carry
+      if (l1) {
+        total += l1.val
+        l1 = l1.next
+      }
+      if (l2) {
+        total += l2.val
+        l2 = l2.next
+      }
+      carry += Math.floor(total / 10)
+      total = total % 10
+      current.next = new ListNode(total)
+      current = current.next
     }
-    if (l2) {
-      sum += l2.val
-      l2 = l2.next
-    }
-
-    carry = Math.floor(sum / 10)
-    sum = sum % 10
-    current.next = new ListNode(sum)
-    current = current.next
-  }
-  if (carry > 0) current.next = new ListNode(carry)
-  return dummyHead.next
+    if (carry >= 1) current.next =new ListNode(carry)
+    return dumbyNode.next
 }
 
-console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]))
+

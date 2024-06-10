19872f086eb50d3c2091cdae151e50caf73d4c57
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-10 23:27:55 +0330

    20th day

--- longest-palindrome-substring-review.js
+++ longest-palindrome-substring-review.js
@@ -1,18 +1,26 @@
 function longestPalindrome(s) {
-  let result
-  function isPal(s, l, r) {
-    while (l >= 0 && r < s.length && s[l] === s[r]) {
-      l++
-      r--
-    }
-    return s.slice(l + 1, r)
-  }
+  let result = ""
 
-  for (let i = 0; i < s.length; i++) {
-    let oddPal = isPal(s, i, i)
-    let evenPal = isPal(s, i, i + 1)
-    let palResult = oddPal.length < evenPal.length ? evenPal : oddPal
-    result = result.length < palResult.length ? palResult : result
+  function isPalindrome(s, leftPointer, rightPointer) {
+    res = ""
+    while (
+      s[leftPointer] === s[rightPointer] &&
+      leftPointer >= 0 &&
+      rightPointer < s.length
+    ) {
+      res = s.slice(leftPointer, rightPointer + 1)
+      leftPointer--
+      rightPointer++
+    }
+    return res
   }
-  result
+  for (let i = 0; i < s.length; i++) {
+    let evenRes = isPalindrome(s, i, i)
+    let oddRes = isPalindrome(s, i, i + 1)
+    let tempRes = evenRes.length > oddRes.length ? evenRes : oddRes
+    result = tempRes.length > result.length ? tempRes : result
+  }
+  return result
 }
+
+longestPalindrome("cbbd")

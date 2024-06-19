9d80b837d277f78a8f106f5d05933a2e2c3f0cf7
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-19 21:58:11 +0330

    29th day

--- 3Sum.js
+++ 3Sum.js
@@ -1,11 +1,10 @@
 var threeSum = function (nums) {
-  if (nums.length === 0) return []
   let res = []
   nums = nums.sort((a, b) => a - b)
   for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue
     let j = i + 1
     let k = nums.length - 1
+    if (nums[i] === nums[i - 1]) continue
     while (j < k) {
       let sum = nums[i] + nums[j] + nums[k]
       if (sum === 0) {
@@ -14,9 +13,9 @@ var threeSum = function (nums) {
         while (nums[k] === nums[k + 1]) k--
         j++
         k--
-      } else if (sum > 0) k--
+      } else if (sum < 0) j++
       else {
-        j++
+        k--
       }
     }
   }

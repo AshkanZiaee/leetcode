7e4aa75ee077b42ca07bae021b8c933a49f5f43a
AshkanZiaee <108191011+AshkanZiaee@users.noreply.github.com>
2024-06-25 23:58:43 +0330

    35th day

--- reverse-int-review.js
+++ reverse-int-review.js
@@ -1,16 +1,15 @@
 var reverse = function (x) {
   let stringifiedNumber = String(x)
   let res = ""
-  let symbol = ""
+  let isNeg = false
   for (let i = stringifiedNumber.length - 1; i >= 0; i--) {
-    if (stringifiedNumber[i] === "-" || stringifiedNumber[i] === "+") {
-      symbol = stringifiedNumber[i]
+    if (stringifiedNumber[i] === "-") {
+      isNeg = true
+      break
     }
     res += stringifiedNumber[i]
   }
-  res = parseInt(symbol + res)
-  if (res > 2 ** 31 || res < 2 ** 31 * -1) return 0
-  return res
+  return isNeg ? -1 * Number(res) : Number(res)
 }
 
 console.log(reverse(-4560))

26a6c50e6cc0e6fb0d87e9ef8c5d41b9471190ee
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-07-03 18:31:48 -1100

    43rd day

--- zigzag.js
+++ zigzag.js
@@ -1,16 +1,19 @@
 function convert(s, numRows) {
   if (numRows === 1) return s;
-  let tempArrays = Array(numRows).fill("");
   let direction = true;
   let count = 0;
+  const rowsArray = Array(numRows).fill("");
+
   for (let i = 0; i < s.length; i++) {
-    tempArrays[count] += s[i];
-    if (direction) count++;
-    else if (!direction) count--;
+    rowsArray[count] = rowsArray[count] + s[i];
+
+    if (direction === true) count++;
+    else if (direction === false) count--;
+
     if (count === numRows - 1) direction = false;
     if (count === 0) direction = true;
   }
-  return tempArrays.join("");
+  return rowsArray.join("");
 }
 
-convert("PAYPALISHIRING", 3);
+console.log(convert("PAYPALISHIRING", 3));

015b87e6865b8384acb2a0982c927f364ddec623
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-07-10 20:10:29 +0200

    49th day

--- zigzag-review.js
+++ zigzag-review.js
@@ -1,20 +1,19 @@
-var convert = function (s, numRows) {
+function convert(s, numRows) {
   if (numRows === 1) return s;
+  let direction = true;
   let count = 0;
-  let turn = true;
-  let res = Array(numRows).fill("");
+  const rowsArray = Array(numRows).fill("");
+
   for (let i = 0; i < s.length; i++) {
-    if (turn) {
-      res[count] = res[count] + s[i];
-      count++;
-    } else if (!turn) {
-      res[count] = res[count] + s[i];
-      count--;
-    }
-    if (count === 0) turn = true;
-    if (count === numRows - 1) turn = false;
+    rowsArray[count] = rowsArray[count] + s[i];
+
+    if (direction === true) count++;
+    else if (direction === false) count--;
+
+    if (count === numRows - 1) direction = false;
+    if (count === 0) direction = true;
   }
-  return res.join("");
-};
+  return rowsArray.join("");
+}
 
 console.log(convert("PAYPALISHIRING", 3));

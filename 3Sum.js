37f94635354eeb8fea5b827cbfad5411fa2ea506
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-07-12 21:07:35 +0000

    51st day

--- 3Sum.js
+++ 3Sum.js
@@ -1,5 +1,5 @@
 var threeSum = function (nums) {
-  let res = [];
+  let result = [];
   nums = nums.sort((a, b) => a - b);
   for (let i = 0; i < nums.length - 2; i++) {
     let j = i + 1;
@@ -8,7 +8,7 @@ var threeSum = function (nums) {
     while (j < k) {
       let sum = nums[i] + nums[j] + nums[k];
       if (sum === 0) {
-        res.push([nums[i], nums[j], nums[k]]);
+        result.push([nums[i], nums[j], nums[k]]);
         while (nums[j] === nums[j + 1]) j++;
         while (nums[k] === nums[k + 1]) k--;
         j++;
@@ -19,7 +19,7 @@ var threeSum = function (nums) {
       }
     }
   }
-  return res;
+  return result;
 };
 
 threeSum([-1, 0, 1, 2, -1, -4]);

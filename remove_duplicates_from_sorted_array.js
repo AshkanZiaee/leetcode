bcb7cbb9d53abc58d040ae16fc3f78637af02a53
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-07-20 18:42:27 +0330

    59th day

---
+++ remove_duplicates_from_sorted_array.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  let k = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
+
+removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);

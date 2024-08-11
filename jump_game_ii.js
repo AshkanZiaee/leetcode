278e1d502e652a2f0761cf05c666373702c6171f
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-08-11 20:29:57 +0330

    81st day

---
+++ jump_game_ii.js
@@ -0,0 +1,16 @@
+var jump = function (nums) {
+  let jumps = 0;
+  let currentEnd = 0;
+  let farthest = 0;
+  for (let i = 0; i < nums.length; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (i === currentEnd) {
+      currentEnd = farthest;
+      jumps++;
+    }
+    if (currentEnd === nums.length - 1) break;
+  }
+  return jumps;
+};
+
+console.log(jump([2, 3, 1, 1, 4]));

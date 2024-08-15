543f1033476dec7ddac8674e150a465efa110dc8
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-08-15 22:43:56 +0330

    85th day

--- jump_game_ii.js
+++ jump_game_ii.js
@@ -1,9 +1,8 @@
 var jump = function (nums) {
-  if (nums.length === 1) return 0;
   let jumps = 0;
   let currentEnd = 0;
   let farthest = 0;
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length - 1; i++) {
     farthest = Math.max(farthest, i + nums[i]);
     if (i === currentEnd) {
       currentEnd = farthest;
---
+++ permutations.js
@@ -0,0 +1,19 @@
+var permute = function (nums) {
+  const result = [];
+  const used = Array(nums.length).fill(false);
+  function backtrack(path) {
+    if (path.length === nums.length) {
+      result.push([...path]);
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i]) continue;
+      used[i] = true;
+      path.push(nums[i]);
+      backtrack(path);
+      path.pop();
+      used[i] = false;
+    }
+  }
+  backtrack([]);
+  return result;
+};

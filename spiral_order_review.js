6994a11cd40b620f222ab9ba8882adf8021f11ad
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-08-29 17:34:17 +0330

    98th day

---
+++ spiral_order_review.js
@@ -0,0 +1,27 @@
+var spiralOrder = function (matrix) {
+  let top = 0;
+  let left = 0;
+  let bottom = matrix.length - 1;
+  let right = matrix[0].length - 1;
+  let size = matrix.length * matrix[0].length;
+  const result = [];
+  while (result.length < size) {
+    for (let i = left; i <= right && result.length < size; i++) {
+      result.push(matrix[top][i]);
+    }
+    top++;
+    for (let i = top; i <= bottom && result.length < size; i++) {
+      result.push(matrix[i][right]);
+    }
+    right--;
+    for (let i = right; i >= left && result.length < size; i--) {
+      result.push(matrix[bottom][i]);
+    }
+    bottom--;
+    for (let i = bottom; i >= top && result.length < size; i--) {
+      result.push(matrix[i][left]);
+    }
+    left++;
+  }
+  return result;
+};

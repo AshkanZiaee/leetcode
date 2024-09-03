cc62c8850e60652ca2d8404a69470c61adf33caf
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-09-03 10:36:23 +0330

    104th day

---
+++ sqrt.js
@@ -0,0 +1,15 @@
+var mySqrt = function (x) {
+  let left = 0;
+  let right = x;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (mid * mid === x) return mid;
+    if (mid * mid < x) left = mid + 1;
+    else {
+      right = mid - 1;
+    }
+  }
+  return right;
+};
+
+console.log(mySqrt(8));

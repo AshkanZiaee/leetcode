de291653a6ac29592498b1488905fd1251bc4975
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-09-14 21:50:57 +0330

    116th day

--- subsets.js
+++ subsets.js
@@ -1,3 +1,19 @@
 var subsets = function (nums) {
-  function backtrack(start, currentArr) {}
+  const result = [];
+
+  function backtrack(index, currentArr) {
+    if (index === nums.length) {
+      result.push([...currentArr]);
+      return;
+    }
+    currentArr.push(nums[index]);
+    backtrack(index + 1, currentArr);
+
+    currentArr.pop();
+    backtrack(index + 1, currentArr);
+  }
+
+  backtrack(0, []);
+
+  return result;
 };

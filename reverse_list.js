63b55b08fc895102eda32abe0923a66cb9ddb7d1
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-09-14 14:22:02 +0330

    115th day

---
+++ reverse_list.js
@@ -0,0 +1,11 @@
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr !== null) {
+    let nextNode = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+  return prev;
+};

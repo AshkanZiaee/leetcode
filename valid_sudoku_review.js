95fa1166541d9bed9daafb538c4893d45e12dcc1
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-10-03 08:36:56 +0330

    133rd day

---
+++ valid_sudoku_review.js
@@ -0,0 +1,56 @@
+var isValidSudoku = function (board) {
+  for (let i = 0; i < 9; i++) {
+    let visitedValuesCol = new Set();
+    let visitedValuesRow = new Set();
+    for (let j = 0; j < 9; j++) {
+      let rowValue = board[i][j];
+      let colValue = board[j][i];
+      if (rowValue !== ".") {
+        if (visitedValuesRow.has(rowValue)) return false;
+        else {
+          visitedValuesRow.add(rowValue);
+        }
+      }
+
+      if (colValue !== ".") {
+        if (visitedValuesCol.has(colValue)) return false;
+        else {
+          visitedValuesCol.add(colValue);
+        }
+      }
+    }
+  }
+
+  for (let boxRow = 0; boxRow < 3; boxRow++) {
+    for (let boxCol = 0; boxCol < 3; boxCol++) {
+      let visitedValuesBox = new Set();
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          let currentValue = board[boxRow * 3 + i][boxCol * 3 + j];
+          if (currentValue !== ".") {
+            if (visitedValuesBox.has(currentValue)) return false;
+            else {
+              visitedValuesBox.add(currentValue);
+            }
+          }
+        }
+      }
+    }
+  }
+  return true;
+};
+
+console.log(
+  isValidSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ]),
+);
+// 0 0, 1 0, 2 0, 3 0, 4 0, 0 1, 1 1, 2, 1

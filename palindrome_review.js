1ed84738a0474b67701fde31d65e1c7a8825eb8a
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-10-16 12:38:45 +0330

    144th day

---
+++ palindrome_review.js
@@ -0,0 +1,11 @@
+var isPalindrome = function (x) {
+  const stringifiedNumber = String(x);
+  let res = 0;
+  for (let i = stringifiedNumber.length - 1; i >= 0; i--) {
+    res *= 10;
+    res += Number(stringifiedNumber[i]);
+  }
+  return res === x;
+};
+
+console.log(isPalindrome(357));

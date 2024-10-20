e1d004fe2e6ec951af756e3f39801f1101d6f040
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-10-20 13:46:22 +0330

    148th day

---
+++ valid_palindrome.js
@@ -0,0 +1,16 @@
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+  let updatedString = "";
+  for (let i = 0; i < s.length; i++) {
+    let charCode = s.charCodeAt(i);
+    if (
+      (charCode >= 97 && charCode <= 122) ||
+      (charCode >= 48 && charCode <= 57)
+    )
+      updatedString += s[i];
+  }
+  console.log(updatedString);
+  return updatedString === updatedString.split("").reverse().join("");
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));

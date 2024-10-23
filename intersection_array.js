b045c783d6833e846380648156072a7dde64e70e
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-10-23 16:51:56 +0330

    151st day

--- intersection_array.js
+++ intersection_array.js
@@ -1,10 +1,13 @@
 var intersection = function (nums1, nums2) {
   const set1 = new Set(nums1);
   let result = [];
+  console.log(set1);
   for (let num of nums2) {
     if (set1.has(num)) result.push(num);
     set1.delete(num);
   }
-
+  console.log(result);
   return result;
 };
+
+intersection([4, 9, 5, 4], [9, 4, 9, 8]);

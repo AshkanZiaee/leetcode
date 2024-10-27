a1a7fd601ce3f74af7b4217f27549d130683b7cc
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-10-27 14:13:46 +0330

    155th day

---
+++ remove_element_review2.js
@@ -0,0 +1,12 @@
+var removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
+
+removeElement([0, 2, 6, 2, 3, 0, 4, 2], 2);

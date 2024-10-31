bdb43e4e708f415b706e579d3cb761a99d412e60
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-10-31 17:23:01 +0330

    157th day

---
+++ minimum_rotated_sorted_array.js
@@ -0,0 +1,13 @@
+var findMin = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return nums[right];
+};

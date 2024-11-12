91a395b47c538550178b71d0198cf64e7c8f47f3
Ashkan Ziaee <ziaee.ash@gmail.com>
2024-11-12 11:12:28 +0330

    168th day

---
+++ linked_list_cycle.js
@@ -0,0 +1,12 @@
+var hasCycle = function (head) {
+  if (head === null) return false;
+  if (head.next === null) return false;
+  let slow = head;
+  let fast = head.next;
+  while (fast !== null && fast.next !== null) {
+    if (slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return false;
+};

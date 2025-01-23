b75dd8a4db94b0e29abe5efb1f0d8e27f68781ea
Ashkan Ziaee <ziaee.ash@gmail.com>
2025-01-23 18:00:31 +0100

    187th day

--- add-two-numbers-review.js
+++ add-two-numbers-review.js
@@ -5,26 +5,38 @@ class ListNode {
   }
 }
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 var addTwoNumbers = function (l1, l2) {
-  let dumbyHead = new ListNode();
+  const dummyHead = new ListNode(0);
+  let current = dummyHead;
   let carry = 0;
-  let current = dumbyHead;
-  while (l1 !== null || l2 !== null) {
-    let sum = carry;
-    if (l1) {
-      sum += l1.val;
-      l1 = l1.next;
-    }
-    if (l2) {
-      sum += l2.val;
-      l2 = l2.next;
-    }
-
+  while (l1 || l2) {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+    const sum = val1 + val2 + carry;
+    const digit = sum % 10;
     carry = Math.floor(sum / 10);
-    sum = sum % 10;
-    current.next = new ListNode(sum);
+    const node = new ListNode(digit);
+    current.next = node;
     current = current.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
   }
-  if (carry >= 1) current.next = new ListNode(carry);
-  return dumbyHead.next;
+  if (carry > 0) {
+    const node = new ListNode(carry);
+    current.next = node;
+  }
+
+  return dummyHead.next;
 };
